7cd60ff701b0d1a92e2e1cdc1993b011ee66d7b6
Rylei Mindrum <rymindrum@gmail.com>
2024-10-28 20:43:41 -0600

    Working git add fib.asm  (Ugly Code)

--- fib.asm
+++ fib.asm
@@ -1,11 +1,61 @@
 .data
+# ASSN PT 1
     space: .asciiz " "
     newline: .asciiz "\n"
 
     array_1: .word 1, 1, 2, 3, 5
     array_2: .word 8, 13, 21
-.text
 
+# ASSN PT 2
+    .align 2
+    fibonacci_array: .space 128 # for 32 ints
+    prompt: .asciiz "Enter Number of Fibonacci Numbers: "
+.text
+.globl main
+main:
+# Part 1
+#    la $a0, array_1
+#    li $a1, 5
+#    jal print_array
+#
+#    la $a0, array_2
+#    li $a1, 3
+#    jal print_array
+#
+#    li $v0, 10
+#    syscall
+
+# Part 2
+    la $a0, prompt
+    li $v0, 4
+    syscall
+
+    li $v0, 5
+    syscall
+
+    move $t0, $v0
+    move $t1, $zero
+
+begin_loop:
+    bge $t1, $t0, end_loop
+
+    sll $t2, $t1, 2
+    la $t3, fibonacci_array
+    add $t3, $t3, $t2
+    sw $t1, 0($t3)
+
+    la $a0, fibonacci_array
+    addi $a1, $t1, 1
+    jal print_array
+
+    addi $t1, $t1, 1
+    j begin_loop
+
+end_loop:
+    li $v0, 10
+    syscall
+
+# ASSN PT 1
 print_array:
     addi $sp, $sp, -8
     sw $t0, 0($sp)
@@ -38,15 +88,3 @@ end_print:
     lw $t1, 4($sp)
     addi $sp, $sp, 8
     jr $ra
-
-main:
-    la $a0, array_1
-    li $a1, 5
-    jal print_array
-
-    la $a0, array_2
-    li $a1, 3
-    jal print_array
-
-    li $v0, 10
-    syscall
